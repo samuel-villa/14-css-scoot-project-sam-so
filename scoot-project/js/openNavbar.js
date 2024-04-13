55f3a4356fec6d87a954817749d8155623c86f18
samuel-villa <ciullasamuel@gmail.com>
2024-04-13 22:57:19 +0200

    [IMP] header and nav menu

--- scoot-project/js/openNavbar.js
+++ scoot-project/js/openNavbar.js
@@ -1,16 +1,18 @@
 function openMenu() {
     const navbar = document.getElementById("navbar");
+    const header = document.getElementById("header");
     const openButton = document.getElementById("icon-hamburger");
     const closeButton = document.getElementById("close-button");
     if (navbar.style.display === "flex") {
-      navbar.style.display = "none";
-      closeButton.style.display = "none";
-      openButton.style.display = "block";
+        navbar.style.display = "none";
+        closeButton.style.display = "none";
+        openButton.style.display = "block";
+        header.style.position = "relative"
     } else {
-      navbar.style.display = "flex";
-      navbar.classList.add("navbar-menu");
-      closeButton.style.display = "block";
-      openButton.style.display = "none";
+        navbar.style.display = "flex";
+        header.style.position = "fixed"
+        navbar.classList.add("navbar-menu");
+        closeButton.style.display = "block";
+        openButton.style.display = "none";
     }
-  }
-  
+}
