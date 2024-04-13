3e9a245884095f14ec37099dd1ba90e012e653ae
samuel-villa <ciullasamuel@gmail.com>
2024-04-13 17:31:36 +0200

    [IMP] separated script

---
+++ scoot-project/js/openNavbar.js
@@ -0,0 +1,16 @@
+function openMenu() {
+    const navbar = document.getElementById("navbar");
+    const openButton = document.getElementById("icon-hamburger");
+    const closeButton = document.getElementById("close-button");
+    if (navbar.style.display === "flex") {
+      navbar.style.display = "none";
+      closeButton.style.display = "none";
+      openButton.style.display = "block";
+    } else {
+      navbar.style.display = "flex";
+      navbar.classList.add("navbar-menu");
+      closeButton.style.display = "block";
+      openButton.style.display = "none";
+    }
+  }
+  
