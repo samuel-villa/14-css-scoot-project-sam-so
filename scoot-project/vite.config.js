f1d99d71be3d615fc0ec6226de20b19733daa66c
samuel-villa <ciullasamuel@gmail.com>
2024-04-15 16:35:15 +0200

    [IMP] deployment

--- scoot-project/vite.config.js
+++ scoot-project/vite.config.js
@@ -7,4 +7,31 @@ export default {
       partialDirectory: resolve(__dirname, 'partials'),
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        second: resolve(__dirname, './html/about.html'),
+        third: resolve(__dirname, './html/careers.html'),
+        fourth: resolve(__dirname, './html/locations.html'),
+      },
+    },
+  },
 };
+
+
+// export default defineConfig({
+//   css: {
+//     devSourcemap: true,
+//   },
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, 'index.html'),
+//         second: resolve(__dirname, 'about.html'),
+//         third: resolve(__dirname, 'careers.html'),
+//         fourth: resolve(__dirname, 'location.html'),
+//       },
+//     },
+//   },
+// });
